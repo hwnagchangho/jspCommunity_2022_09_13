40e1d0d17c43faf4121c677ecb02263d3fd390ad
hwangchangho <changho3755@gmail.com>
2022-09-16 15:00:41 +0900

    12강, req와 resp를 직접 다루기 귀찮으니, Rq 도입

--- src/main/java/org/example/GugudanServlet.java
+++ src/main/java/org/example/GugudanServlet.java
@@ -12,16 +12,15 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet { // extends HttpServlet을 해주어야 doget 오바라이드 사용가능
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //alt + insert => override => doget
-    req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
-    resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
-    resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이라고 알리는 의미
 
-    int dan = Integer.parseInt(req.getParameter("dan")); // parameter를 써주면 /gugudan?dan=숫자 입력해야한다.
-    int limit = Integer.parseInt(req.getParameter("limit"));
+    Rq rq = new Rq(req, resp);
 
-    resp.getWriter().append("<h1>%d단</h1>".formatted(dan)); //<h1></h1> 제목태그 (마크업이라한다.)
+    int dan = rq.getIntParam("dan", 0);
+    int limit = rq.getIntParam("limit", 0);
+
+    rq.appendBody("<h1>%d단</h1>".formatted(dan));
     for (int i = 1; i <= limit; i++) {
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i)); // 기본적으로 div 태그를 많이쓴다.
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
     }
   }
 }
